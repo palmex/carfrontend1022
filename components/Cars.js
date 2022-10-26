b29b2e99630d260fd70a13db90586d68299f6eee
Admin Server <apostlma@ucsd.edu>
2022-10-26 13:29:00 +0000

    added toggle button

--- components/Cars.js
+++ components/Cars.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 
 export default class Cars extends React.Component{
 
     constructor(){
         super(); 
+        this.state = {buttonText : "ON"}
+        this.state = {buttonBool : true}
+    }
+
+
+    doSomething = () => {
+        console.log('Button Pressed')
+        this.setState({buttonBool: !this.state.buttonBool})
+        if(this.state.buttonBool){
+            this.setState({buttonText: "ON"})
+        } else {
+            this.setState({buttonText: "OFF"})
+        }
+        
     }
 
     render(){
         return(
             <View style={styles.cars}>
                 <Text>Car Component</Text>
+                <Button title={this.state.buttonText} onPress={this.doSomething}/>
             </View>
         )
     }
-
 };
 
 const styles = StyleSheet.create({
@@ -23,7 +37,11 @@ const styles = StyleSheet.create({
       flex: 1,
       backgroundColor: '#BBB',
       alignItems: 'center',
+      width: '75%',
+      margin: '5%',
+      padding: '5%',
+      
       justifyContent: 'center',
     }
-  });
+});
 
